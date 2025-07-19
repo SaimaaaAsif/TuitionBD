3db9ca3b19e878361b951921f22cceae226db3f0
Ahasanul Monir Adar <saimaasifbd@gmail.com>
2025-07-20 01:33:28 +0600

    saima

--- app/student/manage-tuition-offers/layout.tsx
+++
@@ -1,3 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-    return <span className="block mt-6">{children}</span>;
-}
--- app/student/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function Student() {
-    redirect("/student/my-tuitions");
-}
